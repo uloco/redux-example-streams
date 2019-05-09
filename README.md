81e14a19d8dc219455637888076d273b6fb2af0a
Umut Topuzoğlu <uloco@schiefewelt.de>
2019-05-09 11:53:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Redux example streams
+
+This is a small example application based on react, redux and redux-thunk.
+
+To start the application, navigate in both directories `api/` and `client/` in
+two different terminals and run `npm install` and `npm start` in each one.
+This will start the mock api server and the react frontend accessing the data.
+
+Then you can start creating, editing and deleting streams.
+
+Api calls are handled by the middleware redux-thunk.
---
+++ client/src/actions/index.js
@@ -0,0 +1,48 @@
+import streams from "../apis/streams";
+import history from "../history";
+import {
+  CREATE_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  SIGN_IN,
+  SIGN_OUT
+} from "./types";
+
+export const signIn = userId => {
+  return { type: SIGN_IN, payload: userId };
+};
+
+export const signOut = () => {
+  return { type: SIGN_OUT };
+};
+
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
+  history.push("/");
+  dispatch({ type: CREATE_STREAM, payload: { ...response.data, userId } });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.patch(`/streams/${id}`, formValues);
+  history.push("/");
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+  history.push("/");
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
